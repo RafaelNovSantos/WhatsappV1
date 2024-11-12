29168c8d4672423d89e7ef08f134ae3eedc53422
systelrafaelnovais <systelrafaelnovais@gmail.com>
2024-11-12 17:24:10 -0300

    Primeiro commit

---
+++ frontend/frontend.Client/Models/Chat.cs
@@ -0,0 +1,9 @@
+﻿namespace frontend.Client.Models;
+
+public class Chat
+{
+   public string Message { get; set; }
+    public string Tag { get; set; }
+   public string AvatarUrl { get; set; }
+    public string Username { get; set; }
+}
