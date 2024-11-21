58f5bf0015395e5a2878b91fb24bd8ff4440554a
rafanov <rafaelnov84@gmail.com>
2024-11-21 17:14:05 -0300

    Ajuste no css para ocultar titulos do toolbar chat

--- frontend/frontend.Client/Program.cs
+++ frontend/frontend.Client/Program.cs
@@ -3,10 +3,14 @@ using MudBlazor.Services;
 using frontend.Client.Models;
 
 
+
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
 builder.Services.AddSingleton<ChatService>();
+
+
 
 await builder.Build().RunAsync();
